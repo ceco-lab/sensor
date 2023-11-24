d67805731d036407aabb403510ebf072e321f76a
Marine Mamin <135592073+MaminM6@users.noreply.github.com>
2023-11-24 11:04:12 +0100

    cleaned

--- src/gc_ms_analysis.R
+++ src/gc_ms_analysis.R
@@ -1,70 +1,58 @@
-
-library(xgboost)
-library(caret)
-library(rpart)
-library(sp)
-library(rayshader)
-library(rgl)
-library(readobj)
 library(vegan)
 library(rfPermute)
-library(randomForest)
-library(ggalt)
 library(ggplot2)
-library(dplyr)
-library(ggtree)
-library(ape)
-library(vegan)
-library(dendextend)
+library(tidyverse)
+library(ggalt)
 library(wesanderson)
-######################################################################
-#######################GC_ms_delprim
+
 setwd("./data")
 
+#Variety="Delprim"
+Variety="Aventicum"
+Variety
 
-GCMS_delprim <- read.csv("GCMS_all_data_delprim.csv",sep=",") ## ad path var
+if (Variety == "Delprim") {
+  GCMS <- read.csv("GCMS_all_data_delprim.csv", sep = ",") # Delprim data
+} else if (Variety == "Aventicum") {
+  GCMS <- read.csv("GCMS_all_data_aventicum.csv", sep = ",") # Aventicum data
+}
 
-
-GCMS_delprim_x  <-GCMS_delprim[,3:ncol(GCMS_delprim)]
-row.names(GCMS_delprim_x) <- GCMS_delprim$sample
-
-
-
-
+GCMS_x <- GCMS[, 3:ncol(GCMS)]
+row.names(GCMS_x) <- GCMS$sample
 
 cols <- wes_palette("Cavalcanti1")
-cols <-cols[c(4,1,5,2)]
+cols <- cols[c(4, 1, 5, 2)]
 cols[4] <- "royalblue4"
 cols[1] <- "darkgreen"
-
-####3 meta mds
-
-cols2 <- cols#sample(hcl.colors(10, "Vik"))
+cols2 <- cols 
 
 
-nmds <- metaMDS(GCMS_delprim_x,distance="gower", autotransform = FALSE)
+nmds <- metaMDS(GCMS_x, distance = "gower", autotransform = FALSE)
 
-nmds_plot<-vegan::scores(nmds)[1] %>% # pcoa %>%# 
-  cbind(GCMS_delprim) %>%
+nmds_plot <- vegan::scores(nmds)[1] %>% # pcoa %>%# 
+  cbind(GCMS) %>%
   ggplot(aes(x = sites.NMDS1, y = sites.NMDS2)) +
-  geom_encircle(aes(group = treatment, color = treatment, fill = treatment), alpha = 0.8,s_shape=0.8) +
+  xlab(label = "NMDS1") +
+  ylab(label = "NMDS2") +
+  geom_encircle(aes(group = treatment, color = treatment, fill = treatment), alpha = 0.8, s_shape = 0.8) +
   geom_point(aes(color = treatment)) +
   annotate("text", x = -0.45, y = 0.4, label = paste0("stress: ", format(nmds$stress, digits = 4)), hjust = 0) +
-  xlim(-0.5,0.6) + ylim(-0.4,0.4) +
-  theme_bw() + scale_fill_manual(values=cols2) + scale_color_manual(values=cols2)
+  xlim(-0.5, 0.6) + ylim(-0.4, 0.4) +
+  theme_bw() + scale_fill_manual(values = cols2) + scale_color_manual(values = cols2)
 
 nmds_plot
-### data supervizer
+ggsave(paste0("NMDS_GC", Variety, ".pdf"), dpi = 600)
 
-GCMS_delprim_rf<-GCMS_delprim[,2:ncol(GCMS_delprim)]
-#GCMS_delprim_rf <- GCMS_delprim_rf[,c(1,10,8)]
-GCMS_delprim_rf$treatment <- as.factor(GCMS_delprim_rf$treatment)
+# PERMANOVA 
+sink(paste0("permanova_GC_",Variety,".txt"))  # Redirect output to a file
+adonis2(GCMS_x ~ GCMS$treatment, permutations = 999, method = "gower")
+sink()  
 
 
+#Random forest 
+GCMS_rf <- GCMS[, 2:ncol(GCMS)]
+GCMS_rf$treatment <- as.factor(GCMS_rf$treatment)
+sink(paste0("rf_GC_",Variety,".txt"))  # Redirect output to a file
+rfPermute(treatment ~ ., data = GCMS_rf, na.action = na.omit, ntree = 1000, num.rep = 150)
+sink()  
 
-#### randomforest 
-
-model_1 = randomForest(treatment~., data = GCMS_delprim_rf, importance = TRUE,ntree=10000)
-
-ozone.rp <- rfPermute(treatment ~ ., data = GCMS_delprim_rf, na.action = na.omit, ntree = 500, num.rep = 100)
-print(ozone.rp)
