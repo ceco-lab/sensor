0b6a9145858be08abd7ccd7ce60c561b20773a2f
Marine Mamin <135592073+MaminM6@users.noreply.github.com>
2023-11-27 21:07:54 +0100

    cleaned

--- src/gc_ms_analysis.R
+++ src/gc_ms_analysis.R
@@ -3,56 +3,53 @@ library(rfPermute)
 library(ggplot2)
 library(tidyverse)
 library(ggalt)
-library(wesanderson)
+
 
 setwd("./data")
 
-#Variety="Delprim"
-Variety="Aventicum"
-Variety
+set.seed(6)
+
+#Select plant variety and load corresponding data 
+
+Variety="Delprim"
+#Variety="Aventicum"
 
 if (Variety == "Delprim") {
-  GCMS <- read.csv("GCMS_all_data_delprim.csv", sep = ",") # Delprim data
+  GCMS <- read.csv("GCMS_Delprim.csv", sep = ",") # Delprim data
 } else if (Variety == "Aventicum") {
-  GCMS <- read.csv("GCMS_all_data_aventicum.csv", sep = ",") # Aventicum data
+  GCMS <- read.csv("GCMS_Aventicum.csv", sep = ",") # Aventicum data
 }
 
-GCMS_x <- GCMS[, 3:ncol(GCMS)]
-row.names(GCMS_x) <- GCMS$sample
+#Keep only features
+GCMS_x <- GCMS[, 2:ncol(GCMS)]
 
-cols <- wes_palette("Cavalcanti1")
-cols <- cols[c(4, 1, 5, 2)]
-cols[4] <- "royalblue4"
-cols[1] <- "darkgreen"
-cols2 <- cols 
-
+#NMDS
+cols <-c("#D8B70A","darkgreen","#972D15","royalblue4")
 
 nmds <- metaMDS(GCMS_x, distance = "gower", autotransform = FALSE)
-
-nmds_plot <- vegan::scores(nmds)[1] %>% # pcoa %>%# 
+nmds_plot <- vegan::scores(nmds)[1] %>%  
   cbind(GCMS) %>%
   ggplot(aes(x = sites.NMDS1, y = sites.NMDS2)) +
   xlab(label = "NMDS1") +
   ylab(label = "NMDS2") +
-  geom_encircle(aes(group = treatment, color = treatment, fill = treatment), alpha = 0.8, s_shape = 0.8) +
+  geom_encircle(aes(group = treatment, color = treatment, fill = treatment), alpha = 0.6, s_shape = 0.8) +
   geom_point(aes(color = treatment)) +
-  annotate("text", x = -0.45, y = 0.4, label = paste0("stress: ", format(nmds$stress, digits = 4)), hjust = 0) +
-  xlim(-0.5, 0.6) + ylim(-0.4, 0.4) +
-  theme_bw() + scale_fill_manual(values = cols2) + scale_color_manual(values = cols2)
+  annotate("text", x = -0.35, y = 0.3, label = paste0("stress: ", round(nmds$stress, digits = 4)), hjust = 0) +
+  xlim(-0.4, 0.5) + ylim(-0.3, 0.3) +
+  theme_bw() + scale_fill_manual(values = cols) + scale_color_manual(values = cols)+
+  theme(legend.position="none")
 
 nmds_plot
-ggsave(paste0("NMDS_GC", Variety, ".pdf"), dpi = 600)
+ggsave(paste0("NMDS_GC", Variety, ".pdf"), width=8, height=8,dpi = 600)
 
 # PERMANOVA 
-sink(paste0("permanova_GC_",Variety,".txt"))  # Redirect output to a file
+sink(paste0("permanova_GC_",Variety,".txt"))  
 adonis2(GCMS_x ~ GCMS$treatment, permutations = 999, method = "gower")
 sink()  
 
-
 #Random forest 
-GCMS_rf <- GCMS[, 2:ncol(GCMS)]
-GCMS_rf$treatment <- as.factor(GCMS_rf$treatment)
+GCMS_x$treatment <- as.factor(GCMS$treatment)
 sink(paste0("rf_GC_",Variety,".txt"))  # Redirect output to a file
-rfPermute(treatment ~ ., data = GCMS_rf, na.action = na.omit, ntree = 1000, num.rep = 150)
+rfPermute(treatment ~ ., data = GCMS_x, na.action = na.omit, ntree = 1000, num.rep = 150)
 sink()  
 
