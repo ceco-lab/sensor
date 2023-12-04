725a9bcee49ac83bdc054e71c422fde45ceb14c8
Marine Mamin <135592073+MaminM6@users.noreply.github.com>
2023-12-05 00:33:44 +0100

    added lm and multicomp

--- src/PTRMS_lab.R
+++ src/PTRMS_lab.R
@@ -3,20 +3,24 @@ library(rfPermute)
 library(ggplot2)
 library(tidyverse)
 library(ggalt)
+library(car)
+library(openxlsx)
+library(broom)
+library(emmeans)
+library(multcomp)
 
 setwd("./data")
 
 set.seed(6)
 
-Variety="Delprim"
-#Variety="Aventicum"
+#Variety="Delprim"
+Variety="Aventicum"
 
 if (Variety == "Delprim") {
   PTRMS <- read.csv("PTR_Lab_Delprim.csv", sep = ",") # Delprim data
 } else if (Variety == "Aventicum") {
   PTRMS <- read.csv("PTR_Lab_Aventicum.csv", sep = ",") # Aventicum data
 }
-
 
 Sample_ID <- unique(PTRMS$Sample_ID)
 matt_90 <-   data.frame(aggregate(PTRMS[,3:38],
@@ -27,6 +31,7 @@ matt_90 <-   data.frame(aggregate(PTRMS[,3:38],
 matt_90_stat <- matt_90
 matt_90_stat$sample <- matt_90_stat$Group.1
 colnames(matt_90_stat)[2] <- "treatment"
+
 matt_90_x  <-matt_90_stat[,3:(ncol(matt_90_stat)-1)]
 row.names(matt_90_x) <- matt_90_stat$sample
 
@@ -60,5 +65,130 @@ matt_90_x$treatment <- as.factor(matt_90_stat$treatment)
 sink(paste0("rf_PTR_",Variety,".txt"))  # Redirect output to a file
 rfPermute(treatment ~ ., data = matt_90_x, na.action = na.omit, ntree = 1000, num.rep = 150)
 sink()  
+
+
+
+
+matt_90_stat$treatment <- factor(matt_90_stat$treatment, levels = c("Control", "CG", "SE", "SF"))
+
+
+
+# Extracting the order of compounds from the original "PTRMS" dataframe
+compound_order <- colnames(PTRMS)[-c(which(colnames(PTRMS) %in% c("Group.1", "sample", "treatment","Sample_ID")))]
+
+
+
+# Your existing code for data manipulation
+Summary <- matt_90_stat %>%
+    dplyr::select(-c(Group.1, sample)) %>%
+    pivot_longer(!c(treatment), names_to = "compounds", values_to = "value") %>%
+    group_by(treatment, compounds) %>%
+    summarise(mean = mean(value), stand.error = sd(value)/sqrt(n()))  # Using n() for simplicity
+
+# Creating a cleaner summary
+Clean_Summary <- Summary %>%
+  filter(treatment %in% c("Control", "CG", "SF", "SE")) %>%
+  mutate(
+    mean = round(mean, 2),  # Round mean to 2 decimal places
+    stand.error = round(stand.error, 2)  # Round standard error to 2 decimal places
+  ) %>%
+  pivot_wider(names_from = "treatment", values_from = c("mean", "stand.error")) %>%
+  arrange(match(compounds, compound_order)) %>%
+  dplyr::select(
+    compounds,
+    starts_with("mean_Control"),
+    starts_with("stand.error_Control"),
+    starts_with("mean_CG"),
+    starts_with("stand.error_CG"),
+    starts_with("mean_SE"),
+    starts_with("stand.error_SE"),
+    starts_with("mean_SF"),
+    starts_with("stand.error_SF")
+  ) %>%
+  unite("Control", starts_with(c("mean_Control", "stand.error_Control")), sep = " ± ") %>%
+  unite("CG", starts_with(c("mean_CG", "stand.error_CG")), sep = " ± ") %>%
+  unite("SE", starts_with(c("mean_SE", "stand.error_SE")), sep = " ± ") %>%
+  unite("SF", starts_with(c("mean_SF", "stand.error_SF")), sep = " ± ") %>%
+  dplyr::select(compounds, Control, CG, SE, SF)
+
+
+
+# Initialize an empty data frame to store comp results
+comp_results_all <- data.frame(compounds = character(), Letters = character(), stringsAsFactors = FALSE)
+anova_results <- data.frame(compounds = character(), p.value = numeric(), statistic = numeric(), stringsAsFactors = FALSE)
+
+
+
+# Loop over each compound
+for (compound in compound_order) {
+  # Create lm model
+  model <- lm(as.formula(paste(compound, " ~ treatment")),  data = matt_90_stat)
+
+  # Perform ANOVA and extract p-value
+  anova_result <- Anova(model, type = "II") %>%
+    tidy() %>%
+    filter(term == "treatment") %>%
+    dplyr::select(p.value, statistic) %>%
+    mutate(compounds = compound)
+    anova_results <- bind_rows(anova_results, anova_result)
+
+  # Check if p-value is less than 0.05
+  if (anova_result$p.value < 0.05) {
+    # Compute emmeans and contrasts only if ANOVA p-value is significant
+    emmeans_result <- emmeans(model, ~ treatment)
+        # Use cld() to obtain letters with  correction
+    cld_result <- cld(emmeans_result, adjust="fdr",Letters = letters,sort=FALSE,reversed=FALSE)
+
+    # Extract letters and append to comp_results_all data frame
+    Comp_letters <- data.frame(
+      compounds = compound,
+      Letters = as.character(cld_result$.group)
+    )
+    comp_results_all <- bind_rows(comp_results_all, Comp_letters)
+  }
+}
+anova_results$p.value <- as.numeric(round((anova_results$p.value),3))
+anova_results$statistic <- as.numeric(round((anova_results$statistic),2))
+
+
+# Reshape comp_results_all to have three columns
+comp_results_all2 <- comp_results_all %>%
+  group_by(compounds) %>%
+  mutate(row_id = row_number()) %>%
+  pivot_wider(names_from = row_id, values_from = Letters) %>%
+  ungroup()
+
+# Merge comp_results_all with Clean_Summary_all
+Clean_Summary_all <- left_join(Clean_Summary, comp_results_all2, by = "compounds") %>%
+  mutate(
+    Control = paste(Control, `1`),
+    CG = paste(CG, `2`),
+    SE = paste(SE, `3`),
+    SF = paste(SF, `4`)
+  ) %>%
+  dplyr::select(-`1`, -`2`, -`3`, -`4`) %>%
+     mutate(
+    Control = str_replace(Control, "NA$", ""),
+    CG = str_replace(CG, "NA$", ""),
+    SE = str_replace(SE, "NA$", ""),
+    SF = str_replace(SF, "NA$", "")
+  )
+
+# Merge the anovCa_results with Clean_Summary
+Clean_Summary_all2 <- left_join(Clean_Summary_all, anova_results, by = "compounds")
+Clean_Summary_all3 <- Clean_Summary_all2 %>%
+  mutate(
+    compounds = paste0(gsub("^X\\.|\\.\\.$", "", compounds), "+"),
+    p.values = case_when(
+      p.value < 0.001 ~ "< 0.001",
+      p.value < 0.01 ~ "< 0.01",
+      TRUE ~ as.character(round(p.value, 3))  # Format other p-values to three decimal places
+    )
+  ) %>%
+  dplyr::select(-p.value) 
+# Writing the combined summary to Excel
+write.xlsx(Clean_Summary_all3, paste0("Table_PTR_Lab", Variety, ".xlsx"))
+
+
 
 
