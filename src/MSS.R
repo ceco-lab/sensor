c52ebd53bd1e5ab0efbac4572295d87c9952137a
Marine Mamin <135592073+MaminM6@users.noreply.github.com>
2023-12-07 19:42:49 +0100

    Final clean and comments

--- src/MSS.R
+++ src/MSS.R
@@ -1,31 +1,34 @@
+# Load necessary libraries
 library(vegan)
 library(rfPermute)
 library(ggplot2)
 library(tidyverse)
 library(ggalt)
 
-
-set.seed(6)
-
+# Set working directory
 setwd("./data")
 
-#Variety="Delprim"
-Variety="Aventicum"
+# Set seed for reproducibility
+set.seed(6)
 
+# Specify the variety
+Variety <- "Aventicum"  # Change to "Aventicum" or "Delprim" if needed
 
+# Read data based on variety
 if (Variety == "Delprim") {
   MSS <- read.csv("MSS_Delprim.csv", sep = ",") # Delprim data
 } else if (Variety == "Aventicum") {
   MSS <- read.csv("MSS_Aventicum.csv", sep = ",") # Aventicum data
 }
-str(MSS)
+
 #Keep only features
 MSS_x <- MSS[, 3:ncol(MSS)]
-row.names(MSS_x) <- MSS$PrimaryID
+str(MSS)
 
-
+MSS$treatment
 cols <-c("#D8B70A","darkgreen","#972D15","royalblue4")
 
+#NMDS
 nmds <- metaMDS(MSS_x,distance="gower")
 
 nmds_plot<- vegan::scores(nmds) %>%
@@ -33,12 +36,14 @@ nmds_plot<- vegan::scores(nmds) %>%
   ggplot(aes(x = NMDS1, y = NMDS2)) +
   geom_encircle(aes(group = treatment, color = treatment, fill = treatment), alpha = 0.8,s_shape=0.8) +
   geom_point(aes(color = treatment)) +
-  annotate("text", x = -1, y = 0.5, label = paste0("stress: ", format(nmds$stress, digits = 4)), hjust = 0) +
+  xlim(-0.5, 0.5) +
+  ylim(-0.2, 0.3) +
+  annotate("text", x = -0.5, y = 0.3, label = paste0("stress: ", format(nmds$stress, digits = 3)), hjust = 0) +
     theme_bw() + scale_fill_manual(values=cols) + scale_color_manual(values=cols)  +
-  theme(legend.position="none")
+  theme(legend.position="none",aspect.ratio=1)
 
 nmds_plot
-ggsave(paste0("NMDS_MSS", Variety, ".pdf"), dpi = 600)
+ggsave(paste0("NMDS_MSS_", Variety, ".pdf"), dpi = 600)
 
 # PERMANOVA 
 sink(paste0("permanova_MSS_",Variety,".txt"))  # Redirect output to a file
